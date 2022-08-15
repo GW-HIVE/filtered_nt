4d838263f39e618a8ddf35f50e2470301f6cc213
Hadley King <hadley_king@gwu.edu>
2022-08-15 17:35:03 -0400

    1

--- ac2taxid_check.py
+++ ac2taxid_check.py
@@ -13,6 +13,9 @@ from argparse import ArgumentParser, SUPPRESS
 from Bio import SeqIO
 import glob
 import csv
+import os
+import sqlite3
+from sqlite3 import Error
 
 __version__="7.0"
 __status__ = "Dev"
@@ -38,10 +41,9 @@ def usr_args():
     required = parser.add_argument_group('required arguments')
     optional = parser.add_argument_group('optional arguments')
 
-    required.add_argument('-p', '--pathlist',
-        help="Path to `accession2taxid` directory. Should contain NCBI"
-        "accession to taxonomy files",
-        default='./output_data/accession2taxid/nucl_*.accession2taxid'
+    required.add_argument('-d', '--database',
+        help="'db.sqlite3' file. Should contain ",
+        # default='./raw_date/taxdump/names.dmp'
         )
 
     required.add_argument('-n', '--nt',
@@ -62,48 +64,64 @@ def usr_args():
         default=SUPPRESS,
         help='show this help message and exit')
 
-    if len(sys.argv) <= 1:
-        sys.argv.append('--help')
+#    if len(sys.argv) <= 1:
+#        sys.argv.append('--help')
 
     return parser.parse_args()
+
+def create_connection(db_file):
+    """Create Connection
+
+    Creates a database connection to the SQLite database
+    specified by the db_file
+
+    Parameters
+    ----------
+    db_file: str
+        file path to the database file to query.
+
+    Returns
+    -------
+        Database connection object or None
+    """
+
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as error:
+        print(error)
+
+    return conn
+
+def get_taxonomy(conn, accession):
+    """get the taxonomy"""
+
+    cursor = conn.cursor()
+    query = ("select * from accession_taxid where accession = ?")
+    cursor.execute(query, (accession,))
+    row = cursor.fetchone()
+    if row is None:
+        return 'not found'
+    else:
+        return row
+
+def check_nt(conn, nt):
+    """
+    """
+
+    for record in SeqIO.parse(nt, "fasta"):
+        accession = record.id
+        # accession = accession.split('.')[0]
+        if get_taxonomy(conn, accession) == 'not found':
+            print(f'No taxid found for: {accession}')
+        else:
+            print(f'GOT EM: {accession}, {get_taxonomy}')
 
 def main():
     """Main Function"""
 
     options = usr_args()
-    print(options)
-	# patList = "/data/projects/targetdbs/filtered-nt/downloads/nucl_*.accession2taxid.2017-05-21"
-
-	# fileList = glob.glob(patList)
-
-	# ntFile = "/data/projects/targetdbs/filtered-nt/downloads/nt.2017-05-21"
-
-	# FW = open("/data/projects/targetdbs/filtered-nt/generated/logfile.step1.txt", "w")
-	# ac2taxid = {}
-	# for fileName in fileList:
-	# 	if fileName.find("nucl_gb.accession2taxid") >= 0:
-	# 		i = 0
-	# 		with open(fileName, 'rb') as csvfile:
-	# 			csvreader = csv.reader(csvfile, delimiter='\t', quotechar='|')
-	# 			for row in csvreader:
-	# 				seqAc = row[0].strip()
-	# 				ac2taxid[seqAc] = 1
-	# 				if i%10000000 == 0:
-	# 					print("Done loading ", fileName, i)
-	# 				i += 1
-
-
-	# i = 0
-	# for record in SeqIO.parse(ntFile, "fasta"):
-	# 	seqAc = record.id
-	# 	seqAc = seqAc.split('.')[0]
-	# 	if seqAc not in ac2taxid:
-	# 		FW.write("No taxid found for: %s\n" % (seqAc))
-	# 	if i%10000000 == 0:
-	# 		print("Done parsing",  i)
-	# 	i += 1
-
-	# FW.close()
+    print(options)            
 
 
 if __name__ == '__main__':
