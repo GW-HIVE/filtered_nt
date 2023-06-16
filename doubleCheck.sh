0cbcfe2612a6aa46876e77a9afcf987a26ddf9b6
Hadley King <hadley_king@gwu.edu>
2023-06-16 09:31:18 -0400

    23.06 update Changes to be committed: 	modified:   README.md 	modified:   doubleCheck.sh 	new file:   notes.txt 	new file:   test.xml

--- doubleCheck.sh
+++ doubleCheck.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
-# Curl for NCBI Trace to retrieve SRR analysis values
-# Need to provide a text file with a list of SRR accessions
-for acc in $(cat ../logs/logfile.accession2taxid_test.txt)
+# Takes the logfile of accessions with missing taxIds from the db.
+# Greps the files in the accession2taxid dir to find missing tax ids.
+
+# to break the if/then
+output=0
+
+for acc in $(cat logs/logfile.accession2taxid.txt)
 do
-	 grep -i $acc raw_data/accession2taxid/dead_*
+    #if output is zero
+    if [[ $output == 0 ]];
+    then
+        # grep result asinged to output
+        output=$(grep -m 1 $acc raw_data/accession2taxid/*)
+        # if the value for output (grep result) is NULL
+        if [ -z "$output" ]
+            then
+            # Line for missing var in file
+            output="no results for $acc"
+        fi
+        # stdout is sent to file
+        echo $output
+    fi
+    output=0
 done
